95c85227f7eabf911559e84b747095055b93f895
VeronikaStafienko <veronastaf@live.ru>
2016-11-14 23:49:24 +0300

    Добавила интерактивную карту

--- js/script.js
+++ js/script.js
@@ -4,21 +4,29 @@ var popup = document.querySelector(".search-conteiner");
 
 var form = popup.querySelector("form");
 var date = popup.querySelector("[name=date-in]");
-var date = popup.querySelector("[name=date-out]");
-var count = popup.querySelectorAll("[name=count");
+var date1 = popup.querySelector("[name=date-out]");
+var count = popup.querySelectorAll("[name=count]");
+var storage = localStorage.getItem("date");
 
 popup.classList.toggle("search-conteiner-none");
 
 link.addEventListener("click", function (event) {
   event.preventDefault();
   popup.classList.toggle("search-conteiner-none");
-  date.focus();
+  if (storage) {
+    date.value = storage;
+    count.focus();
+  } else {
+    date.focus();
+  }
 });
 form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  if (!date.value || !count.value) {
+  if (!date.value || !date1.value || !count[0].value || !count[1].value) {
     event.preventDefault();
+    popup.classList.remove("search-error");
+    popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("search-error");
-
+  } else {
+    localStorage.setItem("date", date.value);
   }
 });
